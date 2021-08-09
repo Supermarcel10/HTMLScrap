e73572f1c26b30adf8f8a394cb5a1d301fd1405a
Marcel <marcelbarlik@gmail.com>
2021-08-09 21:04:56 +0100

    V 4.7.1 - Created config.py

---
+++ config.py
@@ -0,0 +1,51 @@
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+# Directory of your browser.
+# Note: Windows is already supported out of box.
+SelectedBrowser = "/usr/bin/chromedriver"
+
+# Options of how the browser is executed.
+BrowserOptions = ChromeOptions()
+
+BrowserOptions.add_argument("--headless")
+BrowserOptions.add_argument("--disable-gpu")
+
+# A set of URLs the program executes and the type of execution..
+URLs = {"http://memployees.sportsdirectservices.com/Working-Hours": ["PresentWeek", "NextWeek"],
+        "https://extranet.barnetsouthgate.ac.uk/": ["Past30", "PresentWeek", "Next30"]}
+
+# Setting this value will determine if you use Linux Crontab for running your task.
+#           True - Disables while loop for grabbing data every set time
+#           False - Enables while loop, that will grab data every set time
+Crontab = False
+
+# Setting this value will determine if a log file is created for every run.
+FileLogging = True
+
+# Setting this value will determine how many log files will be kept.
+# Oldest log files will be deleted. Fatal crashes will be kept separately.
+FileLogLimit = True
+FileLogHistory = 24
+
+# Setting these values determine the *.csv file titles (names), which can then be imported elsewhere such as a website.
+titles = {
+    "extranet.barnetsouthgate.ac.uk": "college",
+    "memployees.sportsdirectservices.com": "work",
+    "*": "other"
+}
+
+# Set this value to the xpath of where the login data is inserted
+loggedElement = {"extranet.barnetsouthgate.ac.uk": None,
+                 "memployees.sportsdirectservices.com": '//*[@id="dnn_ctr454_ModuleContent"]/div/div[1]/div/h3'
+                 }
+
+# Set this value to toggle the creation of auth files.
+# Default: False
+# False - Disable auth generation if no file is located (recommended)
+# True - Prompt the user to input a username and password, which will be stored for auth.
+AllowAuthCreator = False
+
+# Setting the key to a value will read the key and use it for decryption.
+# If a key is not specified, a key will be asked on every startup.
+# WARNING: SETTING A PERMANENT KEY IS A SAFETY VULNERABILITY!
+key = ""
--- htmlScrap.py
+++ htmlScrap.py
@@ -1,6 +1,5 @@
 from selenium.common.exceptions import WebDriverException as WebDriverException, NoSuchElementException as NoSuchElementException
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -29,57 +28,7 @@ monthsLong = ["January", "February", "March", "April", "May", "June", "July", "A
 CWD = os_getcwd()
 print(CWD)
 
-# Directory of your browser.
-# Note: Windows is already supported out of box.
-SelectedBrowser = "/usr/bin/chromedriver"
-
-# Options of how the browser is executed.
-BrowserOptions = ChromeOptions()
-
-BrowserOptions.add_argument("--headless")
-BrowserOptions.add_argument("--disable-gpu")
-
-# A set of URLs the program executes and the type of execution..
-URLs = {"http://memployees.sportsdirectservices.com/Working-Hours": ["PresentWeek", "NextWeek"],
-        "https://extranet.barnetsouthgate.ac.uk/": ["Past30", "PresentWeek", "Next30"]}
-
-# Setting this value will determine if you use Linux Crontab for running your task.
-#           True - Disables while loop for grabbing data every set time
-#           False - Enables while loop, that will grab data every set time
-Crontab = False
-
-# Setting this value will determine if a log file is created for every run.
-FileLogging = True
-
-# Setting this value will determine how many log files will be kept.
-# Oldest log files will be deleted. Fatal crashes will be kept separately.
-FileLogLimit = True
-FileLogHistory = 24
-
-# Setting these values determine the *.csv file titles (names), which can then be imported elsewhere such as a website.
-titles = {
-    "extranet.barnetsouthgate.ac.uk": "college",
-    "memployees.sportsdirectservices.com": "work",
-    "*": "other"
-}
-
-# Set this value to the xpath of where the login data is inserted
-loggedElement = {"extranet.barnetsouthgate.ac.uk": None,
-                 "memployees.sportsdirectservices.com": '//*[@id="dnn_ctr454_ModuleContent"]/div/div[1]/div/h3'
-                 }
-
-# Set this value to toggle the creation of auth files.
-# Default: False
-# False - Disable auth generation if no file is located (recommended)
-# True - Prompt the user to input a username and password, which will be stored for auth.
-AllowAuthCreator = False
-
-# Setting the key to a value will read the key and use it for decryption.
-# If a key is not specified, a key will be asked on every startup.
-# WARNING: SETTING A PERMANENT KEY IS A SAFETY VULNERABILITY!
-key = ""
-
-
+import config
 
 def console_log(message: str = None, mode: str = "info"):
     now = str(datetime.now())
@@ -130,7 +79,7 @@ def console_log(message: str = None, mode: str = "info"):
     print(fg.white + "[" + col_now + fg.white + "]" + fg.magenta + ": " + col_message)
     del col_now, col_message
 
-    if FileLogging:
+    if config.FileLogging:
         if mode == "error":
             CurrentLog.write(f"!!! [{now}] {message}\n")
             CurrentLog.close()
@@ -212,13 +161,13 @@ def open_datafile(datafile: str):
             return f.readlines()
     except FileNotFoundError:
         console_log("Authentication file not found!", "warn")
-        if AllowAuthCreator:
+        if config.AllowAuthCreator:
             console_log("First time setup initiated...", "warn")
             from getpass import getpass
             with open(f"auth/{datafile}.txt", mode="x+") as f:
-                f.write(str(password_encrypt(bytes(input("Username: "), "utf-8"), key), "utf-8"))
+                f.write(str(password_encrypt(bytes(input("Username: "), "utf-8"), config.key), "utf-8"))
                 f.write("\n")
-                f.write(str(password_encrypt(bytes(getpass("Password: "), "utf-8"), key), "utf-8"))
+                f.write(str(password_encrypt(bytes(getpass("Password: "), "utf-8"), config.key), "utf-8"))
             with open(f"auth/{datafile}.txt", mode="r") as f:
                 return f.readlines()
         return
@@ -235,12 +184,12 @@ def locate_login(url: str):
     # TODO: Redo in the form of an option menu.
     if datafile == "memployees.sportsdirectservices.com":
         try:
-            driver.find_element_by_id("dnn_ctr462_Login_Login_DNN_txtUsername").send_keys(str(password_decrypt(bytes(data[0], "utf-8"), key), "utf-8"))
-            driver.find_element_by_id("dnn_ctr462_Login_Login_DNN_txtPassword").send_keys(str(password_decrypt(bytes(data[1], "utf-8"), key), "utf-8"))
+            driver.find_element_by_id("dnn_ctr462_Login_Login_DNN_txtUsername").send_keys(str(password_decrypt(bytes(data[0], "utf-8"), config.key), "utf-8"))
+            driver.find_element_by_id("dnn_ctr462_Login_Login_DNN_txtPassword").send_keys(str(password_decrypt(bytes(data[1], "utf-8"), config.key), "utf-8"))
 
             console_log("Successfully located fields!")
 
-            return authenticate(url, str(password_decrypt(bytes(data[0], "utf-8"), key), "utf-8"))
+            return authenticate(url, str(password_decrypt(bytes(data[0], "utf-8"), config.key), "utf-8"))
         except InvalidToken:
             console_log("Cryptography key for authentication is invalid! Continuing...", "warn")
         except:
@@ -253,8 +202,8 @@ def locate_login(url: str):
             if week_num > 52:
                 return False
 
-            ping(f"https://%s:%s@extranet.barnetsouthgate.ac.uk/registers/timetable/user/week/{week_num}" % (str(password_decrypt(bytes(data[0], "utf-8"), key), "utf-8"),
-                                                                                                                  str(password_decrypt(bytes(data[1], "utf-8"), key), "utf-8")), True)
+            ping(f"https://%s:%s@extranet.barnetsouthgate.ac.uk/registers/timetable/user/week/{week_num}" % (str(password_decrypt(bytes(data[0], "utf-8"), config.key), "utf-8"),
+                                                                                                                  str(password_decrypt(bytes(data[1], "utf-8"), config.key), "utf-8")), True)
             return True
         except InvalidToken:
             console_log("Cryptography key for authentication is invalid! Continuing...", "warn")
@@ -315,7 +264,7 @@ def longToShortDayName(original):
 
 
 def isotime():
-    for url in URLs:
+    for url in config.URLs:
         fileName = locate_datafile(url)
 
         try:
@@ -325,7 +274,7 @@ def isotime():
         except:
             df = DataFrame(columns=["title", "date", "start_time", "end_time", "extras", "disabled"])
 
-        for execution in URLs[url]:
+        for execution in config.URLs[url]:
             """Execution types: PRESENTWEEK, NEXTWEEK, PASTWEEK, NEXT30 (next 30 weeks) or PAST30 (past 30 weeks)."""
             Year, WeekYear, _ = datetime.today().isocalendar()
 
@@ -333,8 +282,8 @@ def isotime():
                 ping(url)
 
             try:
-                if loggedElement[fileName] is not None:
-                    if driver.find_element_by_xpath(loggedElement[fileName]):
+                if config.loggedElement[fileName] is not None:
+                    if driver.find_element_by_xpath(config.loggedElement[fileName]):
                         pass
             except NoSuchElementException:
                 if locate_login(url):
@@ -344,7 +293,7 @@ def isotime():
                     continue
 
             if execution.lower() == "presentweek":
-                console_log('Attempting to grab present week for profile "%s".' % titles[fileName])
+                console_log('Attempting to grab present week for profile "%s".' % config.titles[fileName])
 
                 try:
                     console_log("Pulling data...")
@@ -381,18 +330,18 @@ def isotime():
                                 date = datetime.strptime("%s %s %s" % ((int(week[0]) + rel) - monthrange(int(week[2]), int(datetime.strptime(week[1], "%b").month))[1],
                                                                        datetime.strptime(week[1], "%b").month + 1, week[2]), "%d %m %Y")
 
-                            df = df.append(DataFrame([[titles[fileName], date, weekdays[day][0], weekdays[day][1], weekdays[day][2], 0]], columns=["title", "date", "start_time",
+                            df = df.append(DataFrame([[config.titles[fileName], date, weekdays[day][0], weekdays[day][1], weekdays[day][2], 0]], columns=["title", "date", "start_time",
                                                                                                                                                    "end_time", "extras", "disabled"]),
                                            ignore_index=True)
 
-                            console_log('Successfully pushed "%s" for "%s"!' % ([date.year, date.month, date.day], titles[fileName]))
+                            console_log('Successfully pushed "%s" for "%s"!' % ([date.year, date.month, date.day], config.titles[fileName]))
                     except:
-                        console_log("Failed whilst pushing %s data!" % titles[fileName], "warn")
+                        console_log("Failed whilst pushing %s data!" % config.titles[fileName], "warn")
                 except:
                     console_log("Failed whilst pulling data!", "warn")
 
             if execution.lower() == "nextweek":
-                console_log('Attempting to grab next week for profile "%s".' % titles[fileName])
+                console_log('Attempting to grab next week for profile "%s".' % config.titles[fileName])
 
                 try:
                     console_log("Pulling data...")
@@ -429,13 +378,13 @@ def isotime():
                                 date = datetime.strptime("%s %s %s" % ((int(week[0]) + rel) - monthrange(int(week[2]), int(datetime.strptime(week[1], "%b").month))[1],
                                                                        datetime.strptime(week[1], "%b").month + 1, week[2]), "%d %m %Y")
 
-                            df = df.append(DataFrame([[titles[fileName], date, weekdays[day][0], weekdays[day][1], weekdays[day][2], 0]], columns=["title", "date", "start_time", "end_time", "extras",
+                            df = df.append(DataFrame([[config.titles[fileName], date, weekdays[day][0], weekdays[day][1], weekdays[day][2], 0]], columns=["title", "date", "start_time", "end_time", "extras",
                                                                                                                               "disabled"]),
                                            ignore_index=True)
 
-                            console_log('Successfully pushed "%s" for "%s"!' % ([date.year, date.month, date.day], titles[fileName]))
+                            console_log('Successfully pushed "%s" for "%s"!' % ([date.year, date.month, date.day], config.titles[fileName]))
                     except:
-                        console_log("Failed whilst pushing %s data!" % titles[fileName], "warn")
+                        console_log("Failed whilst pushing %s data!" % config.titles[fileName], "warn")
                 except Exception:
                     console_log("Failed whilst pulling data!", "warn")
 
@@ -453,7 +402,7 @@ def isotime():
         df.to_csv("data/%s.csv" % fileName, index=False, sep=";", na_rep="-")
 
 
-if FileLogging:
+if config.FileLogging:
     if not os_path.exists("logs"):
         os_mkdir("logs")
         os_mkdir("logs/fatal")
@@ -463,22 +412,22 @@ if FileLogging:
     open(r"logs/%s.log" % CurrentLogName, "x")
     CurrentLog = open(r"logs/%s.log" % CurrentLogName, "a")
 
-    if FileLogLimit:
+    if config.FileLogLimit:
         for files in os_search(r"logs/"):
-            while len(files[2]) > FileLogHistory - 1:
+            while len(files[2]) > config.FileLogHistory - 1:
                 os_rm(min(["logs/{0}".format(f) for f in files[2]], key=os_path.getctime))
                 files[2].pop(0)
 
-if len(key) < 1:
+if len(config.key) < 1:
     from getpass import getpass
-    key = getpass("Key:")
+    config.key = getpass("Key:")
 
 if not os_path.exists("data"):
     os_mkdir("data")
 
 if not os_path.exists("auth"):
     os_mkdir("auth")
-    AllowAuthCreator = True
+    config.AllowAuthCreator = True
     console_log("Enabling first time setup. Creation of authentication files enabled.", "warn")
 
 
@@ -488,8 +437,8 @@ console_log('Running selentium version: "%s".' % webdriver.__version__)
 if sysplatform() == "Windows":
     browser = os_path.realpath(os_getcwd() + "/chromedriver.exe")
 else:
-    if SelectedBrowser:
-        browser = os_path.realpath(SelectedBrowser)
+    if config.SelectedBrowser:
+        browser = os_path.realpath(config.SelectedBrowser)
     else:
         console_log("No browser has been setup!\nExit Code: -1", "error")
         raise WebDriverException()
@@ -508,7 +457,7 @@ console_log("Attempting to run browser...")
 
 try:
     time_start = datetime.now()
-    driver = webdriver.Chrome(options=BrowserOptions, executable_path=browser)
+    driver = webdriver.Chrome(options=config.BrowserOptions, executable_path=browser)
     console_log("Browser successfully executed!")
 except:
     console_log("Browser window crashed or failed to open!", "error")
@@ -526,7 +475,7 @@ except:
 if __name__ == "__main__":
     isotime()
 
-    while not Crontab:
+    while not config.Crontab:
         if (datetime.now().minute == 0) and (datetime.now().second == 0):
             isotime()
         else:
